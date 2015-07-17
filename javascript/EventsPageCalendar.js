37612823156bc20f3cade0cf1559f672100e8915
Stewart Wilson <stewart.wilson@internetrix.com.au>
2015-07-17 15:54:07 +1000

    remove unnecessary console logs and clean up js file.

--- javascript/EventsPageCalendar.js
+++ javascript/EventsPageCalendar.js
@@ -1,9 +1,5 @@
 (function($) {
 	$(function () {
-//		if(useQtips){
-			applyStyles();
-//		}
-		
 		$(".calendar").on("click", "#month-navigator a", function() {
 			var me 	= $(this);
 			var url = me.attr("href");
@@ -11,21 +7,15 @@
 			me.replaceWith('<img style="width: 25px; float:' + float + ';" src="events/images/loading_transparent.gif" />');
 			
 			$(".calendar").load(url, null, function(){
-//				if(useQtips){
 					applyStyles();
-//				}
 			});
 			return false;
 		});
 		
 		function applyStyles()
 		{
-			console.log("apply");
 			jQuery.each(jQuery(".calendar table .has-event a"), function(i, el) {
-				
-//				var title = jQuery(this).next("ul").html();
 				var title = jQuery(this).attr('data-title');
-				console.log(title);
 				
 				jQuery(this).qtip({
 						content: title,

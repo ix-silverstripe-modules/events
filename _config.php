9b83d83054d0356157db008a69200f98030c8b57
Stewart Wilson <stewart.wilson@internetrix.com.au>
2015-07-23 13:17:05 +1000

    change editableformfield has_one relationship

--- _config.php
+++ _config.php
@@ -6,3 +6,13 @@ $dir = basename(dirname(__FILE__));
 if($dir != "events") {
 	user_error('Directory name must be "events" (currently "'.$dir.'")',E_USER_ERROR);
 }
+
+// Change the relationship
+if(class_exists('EditableFormField')) {
+	$editableHasOne = Config::inst()->get('EditableFormField', 'has_one');
+	if(isset($editableHasOne['Parent'])){
+		$editableHasOne['Parent'] = 'Page';
+	}
+	Config::inst()->remove('EditableFormField', 'has_one');
+	Config::inst()->update('EditableFormField', 'has_one', $editableHasOne);
+}

9f6d51cf64dd4fec0c4b4f25379c7bf4151030c2
Stewart Wilson <stewart.wilson@internetrix.com.au>
2015-07-24 14:26:47 +1000

    side calendar can load events in future and past in background

--- javascript/EventsPageCalendar.js
+++ javascript/EventsPageCalendar.js
@@ -2,15 +2,31 @@
 	$(function () {
 		applyStyles();
 		
+		/* jump links */
+		loadCalendarJumps();
+		
 		$(".calendar").on("click", "#month-navigator a", function() {
 			var me 	= $(this);
 			var url = me.attr("href");
 			var float = me.hasClass('calendar-prev')  ? "left" : "right";
-			me.replaceWith('<img style="width: 25px; float:' + float + ';" src="events/images/loading_transparent.gif" />');
 			
-			$(".calendar").load(url, null, function(){
+			var nextmonth = $('.calendar-jump-forward').html();
+			var prevmonth = $('.calendar-jump-back').html();
+			
+			if(nextmonth && float == "right") {
+				$(".calendar").html(nextmonth);
+			}
+			
+			if(prevmonth && float == "left") {
+				$(".calendar").html(prevmonth);
+			}
+			
+			applyStyles();
+			loadCalendarJumps();
+			
+			/*$(".calendar").load(url, null, function(){
 					applyStyles();
-			});
+			});*/
 			return false;
 		});
 		
@@ -36,6 +52,28 @@
 						}
 					}
 				);
+			});
+		}
+		
+		function loadCalendarJumps() {
+			$(".ui-datepicker-title").append('<img class="spinner" style="width: 25px; text-align:center;" src="events/images/loading_transparent.gif" />');
+			
+			/* jump links */
+			var prevurl = $('#month-navigator .calendar-prev').attr('href');
+			var nexturl = $('#month-navigator .calendar-next').attr('href');
+			
+			/* remove jump icons for now */
+			$('#month-navigator .calendar-next').hide();
+			$('#month-navigator .calendar-prev').hide();
+			
+			/* Load Back and Forward in the background*/
+			$(".calendar-jump-forward").load(nexturl, null, function(){
+				$('#month-navigator .calendar-next').show();
+				$(".spinner").remove();
+			}); 
+			$(".calendar-jump-back").load(prevurl, null, function() {
+				$('#month-navigator .calendar-prev').show();
+				$(".spinner").remove();
 			});
 		}
 	})
--- templates/Includes/Calendar.ss
+++ templates/Includes/Calendar.ss
@@ -1,3 +1,9 @@
 <div class="calendar event-cal">
 $eventcalendar
 </div>
+
+<div class="calendar-jump-forward" style="display: none;">
+</div>
+
+<div class="calendar-jump-back" style="display: none;">
+</div>
